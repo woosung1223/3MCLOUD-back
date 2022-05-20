a28e365499fa0e5e9f71479bb709181e971ecc16
dntjd <dntjd991223@naver.com>
2022-05-20 20:48:51 +0900

    Add model, serializer

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    UserId = models.CharField(max_length=100)
+    IdToken = models.TextField() # user pool token
+    # user pool token은 이후에 credential으로 교환
