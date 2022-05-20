f287610257493f78052207b3d3e0c8651f6d6af2
dntjd <dntjd991223@naver.com>
2022-05-20 21:41:59 +0900

    Add skeleton views

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import Http404
 
 # Create your views here.
+class LoginView(APIView):
+    def get(self, request):
+        return HttpResponse("login1")
+
+    def post(self, request):
+        return HttpResponse("login2")
+
+class RegisterView(APIView):
+    def get(self, request):
+        return HttpResponse("register1")
+
+    def post(self, request):
+        return HttpResponse("register2")
+
