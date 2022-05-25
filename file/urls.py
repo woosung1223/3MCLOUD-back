073b7827a39a39dd87adb186d218588aa2e8c9d6
yeonjin <duswls214@khu.ac.kr>
2022-05-25 10:02:32 +0900

    Remove models

--- file/urls.py
+++ file/urls.py
@@ -1,11 +1,14 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import routers
+
 
 urlpatterns = [
-    path("upload/<int:folder_id>", views.uploadFile, name="uploadFile"),
-    path("<int:file_id>/download/", views.downloadFile, name="downloadFile")
+    path("upload/<str:folder_id>", views.uploadFile, name="uploadFile"),
+    path("<int:file_id>/download/", views.downloadFile, name="downloadFile"),
+    path("", views.listFile, name="listFile"),
 ]
 
 if settings.DEBUG:
--- file/views.py
+++ file/views.py
@@ -1,11 +1,12 @@
 import uuid
-from .models import Folder, File
+# from .models import Folder, File
 import os
 from django.http import FileResponse, JsonResponse
 from django.core.files.storage import FileSystemStorage
 import boto3
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
+from urllib.parse import urlparse
 # from django.contrib.auth.models import AbstractUser
 from django.shortcuts import get_object_or_404, render
 
@@ -17,37 +18,24 @@ s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_k
 
 @csrf_exempt
 def uploadFile(request, folder_id):
-    # 폴더 임시 생성 (테스트용)
-    new_folder = Folder(
-        folder_id=folder_id,
-        user_id=123,
-    )
-    new_folder.save()
 
-    folder = get_object_or_404(Folder, folder_id=folder_id) # 폴더 데이터 가져옴
     if request.method == "POST":
         # 여러개 파일 저장
         uploadedFiles = request.FILES.getlist('files')
+        # url = request.POST["url"] # 전체 url
+        # file_path = urlparse(url).path.split("/")
+        file_path = request.POST["file_path"] # 현재 디렉토리 위치
+        print(file_path)
         user_id = "test" #임시 user 생성
         for uploadedFile in uploadedFiles:
             # 예외 처리: 이름 같은 파일 없는지 확인 -> 있으면 추가로 이름 붙여서 저장
             fname, ext = os.path.splitext(str(uploadedFile))
             uuidfilename = str(uuid.uuid1()).replace('-', '')
-            # sqlite 저장
-            new_file = File(
-                file_name=str(uploadedFile),
-                uuid_file_name=uuidfilename,
-                parent=folder,
-                user_id=user_id,
-                extension=ext,
-            )
-            new_file.save()
-
+            # sqlite
             try:
-                s3.upload_fileobj(uploadedFile, AWS_STORAGE_BUCKET_NAME, user_id + "/" + uuidfilename)
+                s3.upload_fileobj(uploadedFile, AWS_STORAGE_BUCKET_NAME, user_id + "/" + file_path + uuidfilename)
             except:
-                # 실패 시 삭제
-                File.objects.get(pk=new_file.id).delete()
+                # 실패 시
                 return JsonResponse({
                     'result': 'Upload failed',
                 })
@@ -56,6 +44,11 @@ def uploadFile(request, folder_id):
         'result': 'Upload succeed',
     })
 
+@csrf_exempt
+def listFile(request):
+
+
+@csrf_exempt
 def downloadFile(request, file_id):
     # file_id로 file 정보 찾기
     # file 정보에서 소유자, 저장 파일명 찾기
